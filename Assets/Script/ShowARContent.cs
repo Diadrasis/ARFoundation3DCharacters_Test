0e598cc75808b0b647ff20458b05e80fd14feb1b
Diadrasis <info@diadrasis.gr>
2021-09-04 21:31:51 +0300

    test charachters

--- Assets/Script/ShowARContent.cs
+++ Assets/Script/ShowARContent.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 public class ShowARContent : MonoBehaviour
 {
     //track the state of the game
-    public enum State {start, chooseModel, showModel, hideModel};
+    public enum State {init, chooseModel, showModel, hideModel};
     //keep the app state
     public State appState;
     //the character model we will show
@@ -20,7 +20,10 @@ public class ShowARContent : MonoBehaviour
     int planes_count=0;   
     Text txtPlanesCount;
     Text txtCameraPosition;
+    Text txtPosePosition;
     Text txtModelName;
+    Text txtAppState;
+    
 
     //the 3d model 
     GameObject model;
@@ -31,7 +34,7 @@ public class ShowARContent : MonoBehaviour
     Toggle t;
 
     void Awake(){
-         appState=State.start;
+         appState=State.init;
     }
     
     void Start()
@@ -41,6 +44,8 @@ public class ShowARContent : MonoBehaviour
         txtCameraPosition=GameObject.Find("txtCameraPosition").GetComponent<Text>();
         txtModelName=GameObject.Find("txtModelName").GetComponent<Text>();
         txtModelName.text="No model set";
+        txtAppState=GameObject.Find("txtAppState").GetComponent<Text>(); 
+        txtPosePosition=GameObject.Find("txtPosePosition").GetComponent<Text>();       
         //define the ARCamera
         arCamera=Camera.main;        
         //define the plane manager
@@ -57,6 +62,7 @@ public class ShowARContent : MonoBehaviour
     void Update()
     {       
         
+        txtAppState.text=appState.ToString();
         txtCameraPosition.text=arCamera.transform.position.ToString();
         if (planeManager.enabled==true){
             planes_count=planeManager.trackables.count;
@@ -69,22 +75,24 @@ public class ShowARContent : MonoBehaviour
         txtPlanesCount.text=planes_count.ToString();
         
         //when clicked
-        if (Input.GetMouseButtonDown(0)){        
+        if (Input.GetMouseButtonDown(0)){
+            if  (appState==State.init){
+                appState=State.showModel;
+            }       
             if (appState==State.showModel){
                 if (!planeManager.enabled){
                     planeManager.enabled=!planeManager.enabled;               
                 }
                 //check which toggle is selected           
                 t=panel.GetComponent<MoveSlidingPanel>().GetSelectedTogle();
-                txtModelName.text=t.name;
+                txtModelName.text=t.GetComponentInChildren<Text>().text;              
                 //Set the prefab according the selected toggle
                 if (t.name=="Toggle1"){                
-                    AR_object=(GameObject)Resources.Load("jasper");
-
+                    AR_object=(GameObject)Resources.Load("jasper");                    
                 } else if (t.name=="Toggle2"){
-                    AR_object=(GameObject)Resources.Load("pearl");
+                    AR_object=(GameObject)Resources.Load("pearl");                   
                 } else {
-                    AR_object=(GameObject)Resources.Load("jasper");   
+                    AR_object=(GameObject)Resources.Load("jasper");                      
                 }
                 //show the model
                 Ray ray = arCamera.ScreenPointToRay(Input.mousePosition);
@@ -94,19 +102,26 @@ public class ShowARContent : MonoBehaviour
                     Vector3 rot=pose.rotation.eulerAngles;
                     //rotation of the ar object, inverted 
                     rot = new Vector3(rot.x, rot.y+180, rot.z); 
-                    //calculate the rotation accordign to the camera              
+                    //place the model on the plane and in spdcific distance of the camera
+                    Vector3 newPos= new Vector3(pose.position.x, pose.position.y, pose.position.z);              
+                    //model=Instantiate(AR_object, newPos, Quaternion.Euler(rot));
                     model=Instantiate(AR_object, pose.position, Quaternion.Euler(rot));
-                    //planeManager.enabled=false;                    
+                    txtPosePosition.text=pose.position.ToString();
+                   
+                    //planeManager.enabled=false; 
+                     //prepare for next click
+                    planeManager.enabled=false;
+                    appState=State.hideModel;                   
                 }
-                //prepare for next click
-                appState=State.hideModel;
-            }else if (appState==State.hideModel){
+               
+            } else if (appState==State.hideModel){
                 //hide the modl
                 Destroy(model);
                 //stop the plane manager
                 if (planeManager.enabled){
                     planeManager.enabled=!planeManager.enabled;                
                 }
+                planes_count=0;
                 //prepare for next click
                 appState=State.showModel;
             }
