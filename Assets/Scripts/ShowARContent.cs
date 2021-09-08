e30da4cf1459b42688e41b06259a554992c0fd27
Giannis Ladas <giannis@diadrasis.gr>
2021-09-08 17:52:05 +0300

    testin process

--- Assets/Scripts/ShowARContent.cs
+++ Assets/Scripts/ShowARContent.cs
@@ -17,7 +17,8 @@ public class ShowARContent : MonoBehaviour
     Camera arCamera;
     bool bVibrate=true;   
     ARPlaneManager planeManager;
-    int planes_count=0;   
+    int planes_count; 
+    int current_planes;  
     Text txtPlanesCount;
     Text txtCameraPosition;
     Text txtPosePosition;
@@ -34,48 +35,64 @@ public class ShowARContent : MonoBehaviour
     Toggle t;
 
     void Awake(){
-         //appState=State.chooseModel;
+         appState=State.recognizePlanes; 
     }
     
     void Start()
     {
         //define the message text
-        txtPlanesCount=GameObject.Find("txtPlanesCount").GetComponent<Text>();
+        /*
+        
         txtCameraPosition=GameObject.Find("txtCameraPosition").GetComponent<Text>();
+        
+        */
+        txtPlanesCount=GameObject.Find("txtPlanesCount").GetComponent<Text>();
         txtModelName=GameObject.Find("txtModelName").GetComponent<Text>();
-        txtModelName.text="No model set";
+        //txtModelName.text="No model set";
         txtAppState=GameObject.Find("txtAppState").GetComponent<Text>(); 
+        /*
         txtPosePosition=GameObject.Find("txtPosePosition").GetComponent<Text>();       
         //define the ARCamera
-        arCamera=Camera.main;        
+        arCamera=Camera.main;
+        */        
         //define the plane manager
         planeManager = GetComponent<ARPlaneManager>();        
         //deactivate
         planeManager.enabled=false;        
+       
         //get the camera
         arCamera=Camera.main; 
         //referebce the panel 
         panel=GameObject.Find("SlidingPanel"); 
-        appState=State.recognizePlanes; 
-        planes_count=0;       
+        
+        planes_count=0;
+        current_planes=0; 
+          
+           
     }
 
     // Update is called once per frame
     void Update()
     {       
         txtAppState.text=appState.ToString();
-        txtCameraPosition.text=arCamera.transform.position.ToString();
+        //txtCameraPosition.text=arCamera.transform.position.ToString();        
+        txtModelName.text=planeManager.enabled.ToString(); 
         txtPlanesCount.text=planes_count.ToString();
+        if (appState==State.recognizePlanes && planeManager.enabled==true){            
+            planes_count=planeManager.trackables.count;
+            if (planes_count>current_planes){           
+                Handheld.Vibrate();
+                appState=State.showModel;
+                planeManager.enabled=false;
+                current_planes=planes_count;                                  
+            }       
+        }     
+        
         //when clicked
         if (Input.GetMouseButtonDown(0)){
-            if (appState==State.recognizePlanes){
-                planeManager.enabled=true;   
-                planes_count=planeManager.trackables.count;
-                if (planes_count>0){
-                    Handheld.Vibrate();
-                    planeManager.enabled=false;
-                    appState=State.showModel;                    
-                 }                                        
+            //Handheld.Vibrate();
+            if (appState==State.recognizePlanes){               
+               planeManager.enabled=true;                                             
             } else if (appState==State.showModel){
                 //check which toggle is selected           
                 t=panel.GetComponent<MoveSlidingPanel>().GetSelectedTogle();
@@ -91,38 +108,36 @@ public class ShowARContent : MonoBehaviour
                 } else {
                     AR_object=(GameObject)Resources.Load("jasper");                      
                 }
-                //show the model
+                //show the model               
                 Ray ray = arCamera.ScreenPointToRay(Input.mousePosition);
                 if(raycastManager.Raycast(ray, hits))
-                {
+                {                   
+                    //find the point on the plane
                     Pose pose = hits[0].pose;
                     Vector3 rot=pose.rotation.eulerAngles;
                     //rotation of the ar object, inverted 
                     rot = new Vector3(rot.x, rot.y+180, rot.z); 
+                    
                     //place the model on the plane and in spdcific distance of the camera
+                    /*
                     Vector3 newPos=arCamera.transform.forward*5;   
-                    newPos=new Vector3(arCamera.transform.forward.x, -0.5f, arCamera.transform.forward.z)*5;           
+                    newPos=new Vector3(arCamera.transform.forward.x, -0.5f, arCamera.transform.forward.z)*5;
+                    */           
                     //model=Instantiate(AR_object, newPos, Quaternion.Euler(rot));
-                    model=Instantiate(AR_object, newPos, Quaternion.Euler(rot));
-                    txtPosePosition.text=pose.position.ToString();
-                   
-                    //planeManager.enabled=false; 
-                     //prepare for next click
-                    planeManager.enabled=false;
-                    appState=State.hideModel;                   
+                    model=Instantiate(AR_object, pose.position, Quaternion.Euler(rot));
+                    //txtPosePosition.text=pose.position.ToString();
+                    //DeactivatePlanes(planeManager);
+                    Handheld.Vibrate();
+                    appState=State.hideModel;
                 }
-               
+                
             } else if (appState==State.hideModel){
                 //hide the modl
                 Destroy(model);
-                //stop the plane manager
-                if (planeManager.enabled){
-                    planeManager.enabled=!planeManager.enabled;                
-                }
-                planes_count=0;
-                //prepare for next click
+                //prepare for next click                
+                Handheld.Vibrate();
                 appState=State.showModel;
             }
         }
-    }
+    }   
 }
